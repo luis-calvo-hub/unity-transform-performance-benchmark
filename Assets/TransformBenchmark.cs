3907ffc16df5647c7053c9c0c47dd1ef5d1f089d
Luis_Resolution <itsluisg@gmail.com>
2023-01-02 14:25:09 +0100

    - Transform benchmarks added for GetComponent and property get.

---
+++ Assets/TransformBenchmark.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+public class TransformBenchmark : MonoBehaviour {
+    public int iterations = 1000000;
+    
+    private void Awake() {
+        Benchmark(iterations, BenchmarkGetComponentTransform);
+        Benchmark(iterations, BenchmarkPropertyTransform);
+    }
+
+    private void Benchmark(int iterations, Action action) {
+        Stopwatch sw = new();
+        sw.Start();
+        for (int i = 0; i < iterations; i++) {
+            action();
+        }
+        sw.Stop();
+        UnityEngine.Debug.Log($"{sw.Elapsed.Milliseconds} Milliseconds has passed on Benchmark: {action.Method.Name}");
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void BenchmarkGetComponentTransform() {
+        _ = GetComponent<Transform>();
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void BenchmarkPropertyTransform() {
+        _ = transform;
+    }
+}
